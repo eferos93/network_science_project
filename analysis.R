440751eb9e23862749297996fe2253fbbb23d7c8
eferos93 <eros.fabrici@gmail.com>
2021-02-05 16:11:19 +0100

    need to fix bug

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-# Title     : TODO
+# Title     :
 # Objective : TODO
 # Created by: eferos93
 # Created on: 05/02/21
@@ -8,3 +8,6 @@ library(tidyverse)
 library(igraph)
 library(ggraph)
 library(tidygraph)
+
+graph <- tbl_graph(nodes = programming_languages_tibble,
+                   edges = influences_tibble, directed = TRUE)
--- clean_data.R
+++ clean_data.R
@@ -1,11 +1,11 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Data Wrangling
+# Objective : Get the data and tidy it
 # Created by: eferos93
 # Created on: 02/02/21
 
 library(pdftools)
 library(stringr)
-
+library(dplyr)
 
 unclean_data <-
   pdf_text("data/rsif20150249supp1.pdf")
@@ -16,7 +16,7 @@ data_as_vector <- data_as_vector[!grepl("^#", data_as_vector)]
 remove_spaces_and_quotes <- function(data_as_vector, nodes_or_edges) {
   data_as_vector[grepl(paste0("^", nodes_or_edges), data_as_vector)] %>%
   sapply(FUN = function(line) { str_replace_all(line, pattern = paste0(nodes_or_edges, ' '), '') }, USE.NAMES = FALSE) %>%
-  sapply(FUN = function(line) { str_replace_all(line, pattern = '\"', ' ') }, USE.NAMES = FALSE) %>%
+  sapply(FUN = function(line) { str_replace_all(line, pattern = '\"', '') }, USE.NAMES = FALSE) %>%
   sapply(FUN = function(line) { str_replace_all(line, pattern = '^\\s+', '') }, USE.NAMES = FALSE) %>%
   sapply(FUN = function(line) { str_replace_all(line, pattern = '\\s+$', '') }, USE.NAMES = FALSE) %>%
   sapply(FUN = function(line) { str_replace_all(line, pattern = '\\s+', ', ') }, USE.NAMES = FALSE)
@@ -26,7 +26,7 @@ nodes <- remove_spaces_and_quotes(data_as_vector, 'Year')
 edges <- remove_spaces_and_quotes(data_as_vector, 'Cite')
 
 programming_languages_tibble <- tibble(
-  programming_language = sapply(nodes, FUN = function (line) { str_extract(line, pattern = '^\\w+') }, USE.NAMES = FALSE),
+  name = sapply(nodes, FUN = function (line) { str_extract(line, pattern = '^\\w+') }, USE.NAMES = FALSE),
   year = sapply(nodes, FUN = function (line) { str_extract(line, pattern = '\\w+$') }, USE.NAMES = FALSE)
 )
 
@@ -36,3 +36,8 @@ influences_tibble <- tibble(
   #by
   to = sapply(edges, FUN = function (line) { str_extract(line, pattern = '\\w+$') }, USE.NAMES = FALSE)
 )
+
+# rm(edges)
+# rm(nodes)
+# rm(data_as_vector)
+# rm(unclean_data)
